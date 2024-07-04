0d157b3c964d06173ef9e8ce0d6a4b581da829df
Yorman Valencia <yormanvale@outlook.com>
2024-07-04 02:28:07 -0500

    avances en ultimo sprint

--- src/app/public/normatividad/normatividad.component.ts
+++ src/app/public/normatividad/normatividad.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ResolucionesService } from 'src/app/services/resoluciones.service';
+
 
 @Component({
   selector: 'app-normatividad',
   templateUrl: './normatividad.component.html',
   styleUrls: ['./normatividad.component.css']
 })
-export class NormatividadComponent {
+export class NormatividadComponent implements OnInit {
+  resoluciones: any[] = [];
 
+  constructor(private resolucionesServices: ResolucionesService , private router: Router) {}
+
+  ngOnInit(): void {
+    this.obtenerResoluciones();
+   }
+
+   obtenerResoluciones() {
+    this.resolucionesServices.getResoluciones().subscribe((data) => {
+      this.resoluciones = data;
+      console.log(data);
+    });
+  }
+
+  verMas(id: number) {
+    this.router.navigate(['/noticia', id]);
+  }
+
+ 
 }
+
